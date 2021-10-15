00bcde340d6fcf0e09fa1cd6f958c78fdf858649
Gautam Gadipudi <gautam.96.gadipudi@gmail.com>
2021-10-15 12:40:02 -0400

    Keep track of frames in a list
    * Include frame_id at Tracker level
    * Include is_log at  Tracker level - to write to file or not.
    
    -- M2

--- src/tracky/tracker.py
+++ src/tracky/tracker.py
@@ -12,12 +12,21 @@ class Tracker:
     # output path
     output_directory = './output/'
 
+    # Write to file?
+    is_log = True
+
+    frame_id = 0
+
+    frames = []
+
     @staticmethod
-    def track(is_log=True):
+    def track():
         ########################
         # prev_frame: corresponds to the frame that called this method
         # prev_prev_frame: corresponds to the frame that called the method that we want to track
         ########################
+        Tracker.frame_id += 1
+
         prev_frame = inspect.currentframe().f_back
         prev_prev_frame = prev_frame.f_back
 
@@ -33,13 +42,17 @@ class Tracker:
                 "code_context": prev_prev_frame_info.code_context
             },
             "datatype": type(prev_frame.f_locals['self']).__name__,
-            "function": prev_frame_info.function
+            "function": prev_frame_info.function,
+            "frame_id": Tracker.frame_id
         }
 
-        print(f'module \"{details["previous_frame"]["module_name"]}\": in function \"{details["previous_frame"]["function"]}\" at line {details["previous_frame"]["line_no"]}')
-        if is_log:
+        print(f'{Tracker.frame_id}: module \"{details["previous_frame"]["module_name"]}\" in function \"{details["previous_frame"]["function"]}\" at line {details["previous_frame"]["line_no"]}')
+
+        Tracker.frames.append(details)
+        if Tracker.is_log:
             output_file_name = f'{details["previous_frame"]["module_name"]}_{Tracker.timestamp}.jsonl'
             output_file_path = f'{Tracker.output_directory}{output_file_name}'
             with open(f'{output_file_path}', mode='a') as f:
                 data = json.dumps(details)
                 f.write(f'{data}\n')
+            
